446d1988e3ee41ed607f8e8ff098a899840bfe85
robzard <pal-vitek1111@mail.ru>
2023-04-13 00:29:52 +0500

    add method "download_titles" (fastapi)

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from fastapi import FastAPI, Query
 from parsing_lenta import ParsingLenta
 
 app = FastAPI()
+pl = ParsingLenta()
 
 
 @app.get('/get_links')
 @app.get('/get_links/{year}/{month}/{day}')
-async def get_links(year: str = None, month: str = None, day: str = None):
-    pl = ParsingLenta(year, month, day)
-    page_html = await pl.get_page_html()
+async def get_links(year: str = None, month: str = None, day: str = None) -> list:
+    url = pl.get_url_for_start_page(year, month, day)
+    page_html = await pl.get_page_html(url)
     links_list = await pl.get_all_links(page_html)
     return links_list
 
+
+@app.post('/download_titles')
+async def download_links_title(urls_list: list) -> dict[str, int | list]:
+    result = await pl.download_pages(urls_list)
+    titles = await pl.get_titles(result)
+    return {'count': len(titles), 'titles': titles}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
--- parsing_lenta/parsing.py
+++ parsing_lenta/parsing.py
@@ -3,42 +3,62 @@ import aiohttp
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 import asyncio
+import numpy as np
+
+user_agent = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.465 (Edition Yx GX)',
+}
+conn = aiohttp.TCPConnector(limit_per_host=3)
 
 
 class ParsingLenta:
-    def __init__(self, year: str = None, month: str = None, day: str = None):
+    def __init__(self):
         self.URL_SITE = 'https://lenta.ru'
-        self.year = year
-        self.month = month
-        self.day = day
-        self.url = self.get_url()
-        print(f"parsing url - {self.url}")
 
-    def get_url(self) -> str:
-        if self.year and self.month and self.day:
-            return f'{self.URL_SITE}/news/{self.year}/{self.month}/{self.day}'
-        return self.URL_SITE
+    @staticmethod
+    async def request(url: str) -> str:
+        print(f'get page html - {url}')
+        async with aiohttp.ClientSession(trust_env=True, headers=user_agent) as session:
+            async with session.get(url) as response:
+                return await response.text()
 
-    async def get_page_html(self) -> str:
-        async with aiohttp.ClientSession() as session:
-            return await self.request(session, self.url)
+    async def get_page_html(self, url) -> str:
+        return await self.request(url)
 
     async def get_all_links(self, html: str) -> list:
-        links_list = []
         soup = await self.get_page_soup(html)
         links = soup.find_all('a', class_=re.compile('card-mini|card-full-news'))
-        for link in links:
-            href = self.URL_SITE + link.get('href')
-            links_list.append(href)
+        links_list = [self.URL_SITE + link['href'] for link in links]
         print(len(links_list))
         return links_list
+
+    async def download_pages(self, urls: list) -> list:
+        result = []
+        tasks = []
+        for url in urls[:10]:
+            task = asyncio.create_task(self.request(url))
+            tasks.append(task)
+            if len(tasks) == 3:
+                task_result = await asyncio.gather(*tasks)
+                result.append(task_result)
+                tasks = []
+                await asyncio.sleep(1)
+        return list(np.concatenate(result))
+
+    async def get_titles(self, html_list: list) -> list:
+        titles_list = []
+        for html in html_list:
+            soup = await self.get_page_soup(html)
+            title = soup.find('h1', class_=re.compile('title')).text
+            titles_list.append(title)
+        return titles_list
+
+    def get_url_for_start_page(self, year: str = None, month: str = None, day: str = None) -> str:
+        if year and month and day:
+            return f'{self.URL_SITE}/news/{year}/{month}/{day}'
+        return self.URL_SITE
 
     @staticmethod
     async def get_page_soup(html: str) -> BeautifulSoup:
         soup = BeautifulSoup(html, 'html.parser')
         return soup
-
-    @staticmethod
-    async def request(session: ClientSession, url: str) -> str:
-        async with session.get(url) as response:
-            return await response.text()
